3dd061d583d661acf9f39356cabd8a2f9f9c9c76
mahmud aremu <aremumahmud2003@gmail.com>
2023-12-26 23:27:23 +0100

    added the footer

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import '../css/cards.css'; // You may need to create a CSS file for styling
+import ScrollReveal from 'scrollreveal';
 
 const ParallaxImage = ({tags , title , image}) => {
-  const [{ offset }, set] = useSpring(() => ({ offset: 0 }));
+  const [{ offset }, set] = useSpring(() => ({ offset: 0 })); 
 
   const handleScroll = () => {
     set({ offset: window.scrollY });
@@ -16,9 +17,26 @@ const ParallaxImage = ({tags , title , image}) => {
     };
   }, []);
 
+   React.useEffect(() => {
+    // Configure the scroll reveal animation
+    ScrollReveal().reveal('.parallax-image', {
+      delay: 200,
+      origin: 'bottom',
+      distance: '20px',
+      duration: 800,
+      scale: 1.2,
+    });
+  }, []);
   return (
     <div className="parallax-container">
-      <div className="info">
+      <animated.div
+        className="parallax-image"
+        style={{
+          transform: offset.interpolate((o) => `translateY(${o * 0.1}px)`),
+          backgroundImage: `url(${image})`
+        }}
+      >
+       <div className="info">
         <div className="title">{title|| ''}</div>
         <div className="tags">
           {
@@ -26,13 +44,7 @@ const ParallaxImage = ({tags , title , image}) => {
           }
         </div>
       </div>
-      <animated.div
-        className="parallax-image"
-        style={{
-          transform: offset.interpolate((o) => `translateY(${o * 0.1}px)`),
-          backgroundImage: `url(${image})`
-        }}
-      />
+      </animated.div>
     </div>
   );
 };
