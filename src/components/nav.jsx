a56b7630cf85f473115de033bc707e691c2f1898
Aremu mahmud <74737123+aremumahmud@users.noreply.github.com>
2023-12-30 03:26:16 +0100

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,11 +7,11 @@ function Nav({isopen}){
             <ul>
                 <li><a href='/emergency_responders'>Emergency Responders</a></li>
                 <li><a href='/vku'>VKU</a></li>
-                <li><a href='/automobile'>Automobile</a></li>
-                <li><a href='#'>Taxi</a></li>
+                
                 <li><a href='/checktrip'>Check trip</a></li>
                 <li><a href='/moto'>Moto</a></li>
                 <li><a href='/pricing'>Pricing</a></li>
+                <li><a href='#'>Contact Us</a></li>
                 <li><a href='#'>About Us</a></li>
                 <li><a href='#'>Registration</a></li>
             </ul>
