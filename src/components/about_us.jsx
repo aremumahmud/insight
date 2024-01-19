ecaf2876cddcdb4ebb8f763d83daa50efc1351ff
mahmud aremu <aremumahmud2003@gmail.com>
2024-01-19 20:25:50 +0100

    whats the worst that can happen

--- src/components/about_us.jsx
+++ src/components/about_us.jsx
@@ -49,9 +49,10 @@ attributes:'Spieglein Spieglein an der Wand, wer ist die schönste Fahrlehrerin 
 function AboutUs({theme}){
     return (
         <>
-          <Jumbo theme={theme}  image={card} text={'About Us'} />
-          <Article theme={theme}  bottom={true}  title={'Super Driving Instructor For Super Learner Drivers .'} content={['We have set the goal of making driver training possible for everyone, regardless of their financial situation']}/>
-          <Article theme={theme}  top={true} title={'Our Creative Team .'} content={[`The Star driving school only works with selected driving instructors and instructors. With the driving instructors and instructors, we make sure that everyone is specialized in their area and very strong. This is how we ensure high quality and can continue to offer our students a high standard. With a relaxed, collegial and humorous atmosphere, you can quickly reach your goal with a lot of fun and humor.`]} />
+          <Jumbo theme={theme}  image={card} text={'Über uns'} />
+          <Article theme={theme}  bottom={true}  title={'Deine Fahrschule in Aarau, Baden, Winterthur und Zürich .'} 
+          content={['Wir haben zum Ziel gesetzt jedem, unabhängig von seiner finanziellen Situation, die Fahrausbildung zu ermöglichen.']}/>
+          <Article theme={theme}  top={true} title={'Unser Superteam .'} content={[`Die Fahrschule Star arbeitet nur mit ausgewählten Fahrlehrern und Instruktoren zusammen. Bei den Fahrlehrern und Instruktoren achten wir darauf, dass jeder in seinem Gebiet spezialisiert und sehr stark ist. So gewährleisten wir eine hohe Qualität und können unseren Schülern weiterhin einen hohen Standard bieten. Durch eine lockere, kollegiale und humorvolle Atmosphäre kommt man bei uns mit viel Spass und Humor rasch ans Ziel.`]} />
           {
             team_data.map(team=> <Team theme={theme}  phone={team.phone} email={team.email} name={team.name} attributes={team.attributes} image={team.image} />)
           }
